1673c3988530e19e797a15b3c4a9106e20036e32
LJH <hosboy93@naver.com>
2023-04-24 16:45:59 +0900

    preparedStatement Select

--- JDBC/src/Ex08_Oracle_preparedStatement.java
+++ JDBC/src/Ex08_Oracle_preparedStatement.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+import kr.or.kosa.utils.SingletonHelper;
+
 /**
  * (1) 설명
  * 미리 SQL문이 셋팅된 Statement가 DB가 전송 되어져서 컴파일 되어지고, 
@@ -25,6 +32,49 @@
  */
 public class Ex08_Oracle_preparedStatement {
 	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
+		try {
+			conn = SingletonHelper.getConnection("oracle");
+			String sql = "select empno, ename from dmlemp where deptno = ?"; // where deptno = 20
+			// where id = ? and name = ? and job = ? >> where id = 100 and name = '김유신' and job = 'IT'
+			// ? >> parameter
+			
+			/*
+			 * String sql = "select empno, ename, sal, comm from emp";
+			 * ResultSet rs = stmt.executeQuery(sql);
+			 */
+			
+			pstmt = conn.prepareStatement(sql); // preparedStatement가 만들어질 때 미리 컴파일 쿼리를 갖는다
+												// 내가 실행하려는 쿼리는 DB서버가 공유 풀에 가지고 있게 된다
+												// 대신 쿼리문이 바뀌면 여기서부터 다시 실행시켜야 한다는 단점이 있다
+			
+			Scanner sc = new Scanner(System.in);
+			int deptno = Integer.parseInt(sc.nextLine());
+			
+			// where deptno = ?
+			pstmt.setInt(1, deptno); // 첫번째 인덱스는 물음표의 순번, 두번째 인덱스는 물음표의 값, where deptno = 30
+			rs = pstmt.executeQuery(); // 파라미터만 서버에 전달
+			
+			// 공식같은 로직
+			// 데이터가 1건 or 1건 이상 or 없는 경우도 사용 가능
+			if(rs.next()) {
+				// 1건 또는 1건 이상
+				do {
+					System.out.println(rs.getInt(1) + " / " + rs.getString(2));
+				}while(rs.next());
+			}else {
+				// 데이터가 없는 경우
+				System.out.println("조회된 데이터가 없습니다.");
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			SingletonHelper.close(pstmt);
+			SingletonHelper.close(conn);
+			// Singleton은 close() 하지 않는다(App이 살아있는 동안 같은 운명)
+		}
 	}
 }
