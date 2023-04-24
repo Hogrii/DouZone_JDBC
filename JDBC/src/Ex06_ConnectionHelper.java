68f89017ef21077713161ec027d31e5e675a0b28
LJH <hosboy93@naver.com>
2023-04-24 14:57:21 +0900

    Connection Helper 오버로딩

--- JDBC/src/Ex06_ConnectionHelper.java
+++ JDBC/src/Ex06_ConnectionHelper.java
@@ -11,5 +11,12 @@ public class Ex06_ConnectionHelper {
 		System.out.println(conn.getMetaData().getDatabaseProductName()); // 데이터베이스 이름(Oracle, MariaDB 등등)
 		System.out.println(conn.getMetaData().getDatabaseProductVersion()); // 데이터베이스 버전
 		System.out.println(conn.isClosed()); // 데이터베이스 연결여부
+		
+		ConnectionHelper.close(conn);
+		System.out.println(conn.isClosed());
+		
+		conn = ConnectionHelper.getConnection("oracle", "HR", "1004");
+		System.out.println(conn.toString()); // oracle.jdbc.driver.T4CConnection@58a9760d
+		
 	}
 }
