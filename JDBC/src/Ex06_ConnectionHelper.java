8c1d2d8ba34670aa87dd50c5d64c9baf5356d88a
LJH <hosboy93@naver.com>
2023-04-24 16:03:34 +0900

    JDBC Singleton

--- JDBC/src/Ex06_ConnectionHelper.java
+++ JDBC/src/Ex06_ConnectionHelper.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 
 import kr.or.kosa.utils.ConnectionHelper;
+import kr.or.kosa.utils.SingletonHelper;
 
 public class Ex06_ConnectionHelper {
 	public static void main(String[] args) throws Exception{
@@ -15,8 +16,13 @@ public class Ex06_ConnectionHelper {
 		ConnectionHelper.close(conn);
 		System.out.println(conn.isClosed());
 		
-		conn = ConnectionHelper.getConnection("oracle", "HR", "1004");
+		conn = ConnectionHelper.getConnection("oracle");
 		System.out.println(conn.toString()); // oracle.jdbc.driver.T4CConnection@58a9760d
+		
+		// getConnection 요청시마다 새로운 객체가 생성된다.
+		// 현업(Connection Pool) >> 미리 연결 객체를 생성하고 >> 가져다 쓰고 반환하는 방식
+		
+		// 복습(Singleton)
 		
 	}
 }
